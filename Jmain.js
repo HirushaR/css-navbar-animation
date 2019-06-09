77c4f6ae79daff119a9c6f3a8a69ec7d13138039
HirushaR <hirusharandunu11@gmail.com>
2019-06-09 10:26:59 +0530

    add the all files

---
+++ Jmain.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+  if($(this).scrollTop()>1)
+  {
+    $("#main").addClass('shrink');
+  }
+  else
+  {
+    $("#main").removeClass('shrink');
+  }
+});
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # css-navbar-animation
-animation nav bar and dropdown with css js
+animation nav bar and dropdown with css jquery
+jquery-1.10.2 i donwloaded
